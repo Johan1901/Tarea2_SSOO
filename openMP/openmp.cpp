69dfe566f04a952158ff946567ffc7667a6ff993
felipe <felipe.burgos1901@alumnos.ubiobio.cl>
2023-12-03 12:58:56 -0300

    correcion escala de grises

--- openMP/openmp.cpp
+++ openMP/openmp.cpp
@@ -5,32 +5,25 @@ using namespace cv;
 using namespace std;
 
 int main() {
-    Mat image = imread("../images/bosque.jpg", IMREAD_COLOR); //Leer la imagen
+    Mat image = imread("../images/bosque.jpg", IMREAD_COLOR); // Leer la imagen
 
     if (image.empty()) {
-        cout << "No se pudo cargar la imagen." << endl; //Verificar que la imagen se haya cargado correctamente
+        cout << "No se pudo cargar la imagen." << endl; // Verificar que la imagen se haya cargado correctamente
         return -1;
     }
 
     Mat grayImage;
-    cvtColor(image, grayImage, COLOR_BGR2GRAY);//Convertir la imagen a escala de grises
+    cvtColor(image, grayImage, COLOR_BGR2GRAY); // Convertir la imagen a escala de grises
 
-    //Obtener el número de filas y columnas de la imagen
+    // Obtener el número de filas y columnas de la imagen
     int rows = grayImage.rows;
     int cols = grayImage.cols;
 
-    #pragma omp parallel for collapse(2)//Paralelizar el ciclo for
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            uchar* pixel = grayImage.ptr<uchar>(r, c);
-            *pixel = 255 - *pixel;
-        }
-    }
-
-    namedWindow("Imagen procesada", WINDOW_AUTOSIZE);//Crear una ventana para mostrar la imagen
-    imshow("Imagen procesada", grayImage);//Mostrar la imagen
+    namedWindow("Imagen en escala de grises", WINDOW_AUTOSIZE); // Crear una ventana para mostrar la imagen
+    imshow("Imagen en escala de grises", grayImage); // Mostrar la imagen
 
     waitKey(0);
 
     return 0;
 }
+
