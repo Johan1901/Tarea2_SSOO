41a8f9400ae4f5d116ad8bcc7377d3d6efbeef73
felipe <felipe.burgos1901@alumnos.ubiobio.cl>
2023-12-03 13:51:54 -0300

    Ultimos cambios a API OPENMP y tiempo

--- openMP/openmp.cpp
+++ openMP/openmp.cpp
@@ -13,28 +13,30 @@ int main() {
     Mat image = imread("../images/bosque.jpg", IMREAD_COLOR); // Leer la imagen
 
     if (image.empty()) {
-        cout << "No se pudo cargar la imagen." << endl; // Verificar que la imagen se haya cargado correctamente
+        cout << "No se pudo cargar la imagen." << endl;
         return -1;
     }
 
-    Mat grayImage(image.rows, image.cols, CV_8UC1); // Crear una imagen en escala de grises
+    Mat grayImage(image.rows, image.cols, CV_8UC1);
 
-    // Convertir la imagen a escala de grises en paralelo utilizando OpenMP
-    #pragma omp parallel for
+    // Convertir la imagen a escala de grises utilizando el método de luminosidad en paralelo con OpenMP
+    #pragma omp parallel for collapse(2)
     for (int i = 0; i < image.rows; ++i) {
         for (int j = 0; j < image.cols; ++j) {
             Vec3b pixel = image.at<Vec3b>(i, j);
-            grayImage.at<uchar>(i, j) = static_cast<uchar>((pixel[0] + pixel[1] + pixel[2]) / 3);
+            uchar gray_value = 
+            static_cast<uchar>(0.2126 * pixel[2] + 0.7152 * pixel[1] + 0.0722 * pixel[0]);
+            #pragma omp critical
+            grayImage.at<uchar>(i, j) = gray_value;
         }
     }
 
-    // Mostrar el tiempo de compilación en segundos con precisión de hasta 6 decimales
     auto end_time = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count() / 1e6;
     cout << fixed << setprecision(6) << "Tiempo de compilación: " << duration << " segundos" << endl;
 
-    namedWindow("Imagen en escala de grises", WINDOW_AUTOSIZE); // Crear una ventana para mostrar la imagen
-    imshow("Imagen en escala de grises", grayImage); // Mostrar la imagen
+    namedWindow("Imagen en escala de grises", WINDOW_AUTOSIZE);
+    imshow("Imagen en escala de grises", grayImage);
 
     waitKey(0);
 
