05ad7caf629e81043feb12837e21dcd9acc9570c
felipe <felipe.burgos1901@alumnos.ubiobio.cl>
2023-12-03 12:40:40 -0300

    Se añade codigo para mostrar imagen en escala de grises utilizando api openMP

--- openMP/openmp.cpp
+++ openMP/openmp.cpp
@@ -5,18 +5,32 @@ using namespace cv;
 using namespace std;
 
 int main() {
-    // read the image file
-    Mat image = imread("../images/manzanas.jpg", IMREAD_COLOR);
+    Mat image = imread("../images/bosque.jpg", IMREAD_COLOR); //Leer la imagen
 
-    // read each pixel (RGB pixel)
-    for (int r = 0; r < image.rows; r++) {
-        for (int c = 0; c < image.cols; c++) {
-            Point3_<uchar>* p = image.ptr<Point3_<uchar>>(r, c);
-            // x: B, y: G, z: R
-            printf("(%d %d %d) ", p->x, p->y, p->z);
+    if (image.empty()) {
+        cout << "No se pudo cargar la imagen." << endl; //Verificar que la imagen se haya cargado correctamente
+        return -1;
+    }
+
+    Mat grayImage;
+    cvtColor(image, grayImage, COLOR_BGR2GRAY);//Convertir la imagen a escala de grises
+
+    //Obtener el número de filas y columnas de la imagen
+    int rows = grayImage.rows;
+    int cols = grayImage.cols;
+
+    #pragma omp parallel for collapse(2)//Paralelizar el ciclo for
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            uchar* pixel = grayImage.ptr<uchar>(r, c);
+            *pixel = 255 - *pixel;
         }
     }
 
+    namedWindow("Imagen procesada", WINDOW_AUTOSIZE);//Crear una ventana para mostrar la imagen
+    imshow("Imagen procesada", grayImage);//Mostrar la imagen
+
+    waitKey(0);
+
     return 0;
 }
-
