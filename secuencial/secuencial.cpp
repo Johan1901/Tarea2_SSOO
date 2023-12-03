830be4b0d168d60ec3d4dadeb4b46147ee81933c
felipe <felipe.burgos1901@alumnos.ubiobio.cl>
2023-12-03 13:48:13 -0300

    añade codigo principal de secuencial, con tiempo

--- secuencial/secuencial.cpp
+++ secuencial/secuencial.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <chrono>
+#include <iomanip>
 
 using namespace cv;
 using namespace std;
 
 int main() {
+    // Capturar el tiempo de inicio
+    auto start_time = chrono::high_resolution_clock::now();
+
     // Leer la imagen en color
-    Mat image = imread(" ", IMREAD_COLOR); // Poner la ruta de la imagen en los paréntesis
+    Mat image = imread("../images/bosque.jpg", IMREAD_COLOR);
 
     // Verificar si se pudo cargar la imagen
-    if (image.empty()) { 
+    if (image.empty()) {
         cout << "No se pudo cargar la imagen" << endl;
         return -1;
     }
@@ -30,9 +35,19 @@ int main() {
         }
     }
 
-    // Mostrar y guardar la imagen en escala de grises
+    // Capturar el tiempo de finalización
+    auto end_time = chrono::high_resolution_clock::now();
+
+    // Calcular la duración e imprimir el tiempo de ejecución en segundos con 6 decimales
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count() / 1e6;
+    cout << fixed << setprecision(6) << "Tiempo de ejecución: " << duration << " segundos" << endl;
+
+    // Mostrar la imagen en escala de grises en una ventana emergente
+    namedWindow("Imagen en escala de grises", WINDOW_AUTOSIZE);
     imshow("Imagen en escala de grises", image_gray);
-    imwrite("imagen_gris.jpg", image_gray);
+
+    // Esperar hasta que se presione una tecla
+    waitKey(0);
 
     return 0;
 }
